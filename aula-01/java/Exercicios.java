3e82634ffeff55ad3d87599b85fd9951c1f11c7d
kyriosdata <fabio@inf.ufg.br>
2019-04-18 15:20:37 -0300

    condicoes como funcoes

--- aula-01/java/Exercicios.java
+++ aula-01/java/Exercicios.java
@@ -25,7 +25,7 @@ public class Exercicios {
     }
 
     public int diaDaSemana(int d, int m, int a) {
-        if (d < 1 || d > 31 || m < 1 || m > 12 || a < 1753)
+        if (diaInvalido(d) || m < 1 || m > 12 || a < 1753)
             throw new IllegalArgumentException();
         if (m == 1 || m == 2) {
             m = m + 12;
@@ -35,6 +35,10 @@ public class Exercicios {
         int s = d + 2 * m + 3 * (m + 1) / 5 + a + a / 4 - a / 100 + a / 400;
 
         return s % 7;
+    }
+
+    private boolean diaInvalido(int d) {
+        return d < 1 || d > 31;
     }
 
     public int mod(int x, int y) {
