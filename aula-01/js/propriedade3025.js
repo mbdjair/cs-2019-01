42cdff9efb45f4938d89054eed7059a7e18188c0
Djair J M Batista <mbdjair@gmail.com>
2019-04-25 21:39:13 -0300

    Ajuste primeiro arquivo js

---
+++ aula-01/js/propriedade3025.js
@@ -0,0 +1,11 @@
+function propriedade3025(n) {
+    if (n < 0 || n > 9999) {
+        throw new RangeError("argumento fora da faixa");
+    }
+
+    const i = Math.floor(n / 100);
+    const j = n % 100;
+    const soma = i + j;
+
+    return  (soma * soma) == n;
+}
