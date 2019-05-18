8a3ac71774e3d5bf6678a6194ab0a6ebf898c945
Djair J M Batista <mbdjair>
2019-05-18 14:13:59 -0300

    Criação js doc e test cpf2.js

--- aula-01/js/cpf2.js
+++ aula-01/js/cpf2.js
@@ -1,26 +1,45 @@
-import { mod } from "./mod";
+const mod = require("./mod");
 
-function cpf2(d) {
+function stringToIntArray(text) {
+    let intArray = [];
+    for (let i = 0; i < text.length; i++) {
+        intArray[i] = parseInt(text[i]);
+    }
+    return intArray;
+}
 
-    if (!(d instanceof Array)) {
-        throw new TypeError("Tipo errado para o argumento d");
+/**
+ * Algoritmo que retorna se o CPF é válido
+ * @param {Number} n CPF a ser verificado
+ * @returns {boolean} caso CPF seja valido retorna true, caso contrário false
+ * @throws {RangeError} caso n não tenha 11 dígitos
+ * @throws {TypeError} caso entrada não seja uma string
+ */
+
+function cpf2(n) {
+
+    if (typeof n != "string") {
+        throw new TypeError("Tipo errado para o argumento n");
     }
 
-    if (d.length !== 11) {
-        return false;
+    if (n.length !== 11) {
+        throw new RangeError("n precisa pode conter 11 dígitos");
     }
 
+    const a = stringToIntArray(n);
     let c = 7;
-    let p = d[8];
-    let s = d[8];
+    let p = a[8];
+    let s = a[8];
     while (0 <= c) {
-        p = p + d[c];
+        p = p + a[c];
         s = s + p;
         c = c - 1;
     }
 
     const j = mod(mod(s, 11), 10);
-    const k = mod(mod(s - p + 9 * d[9], 11), 10);
+    const k = mod(mod(s - p + 9 * a[9], 11), 10);
 
-    return j === d[9] && k === d[10];
+    return j === a[9] && k === a[10];
 }
+
+module.exports = cpf2;
