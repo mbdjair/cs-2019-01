7f72d73406621cef7fcfa912b8230267f6c3fe15
Djari J M Batista <mbdjair@gmail.com>
2019-07-09 21:04:27 -0300

    Ajuste js da aula 11 e documentação

--- aula-11/gui/js/main.js
+++ aula-11/gui/js/main.js
@@ -1,48 +1,83 @@
 // Path para a requisição (URL)
-const PATH = "http://localhost:8080/ds?data=";
-
+const PATH = "http://localhost:8080/diferencaData?";
+/**
+ * Solicita o diferença de dias e atualiza na view
+ *
+ */
 function atualizaDiaDaSemana() {
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        let dds = extraiDiaDaSemanaDaResposta(xhttp.responseText);
-        document.getElementById("resultado").innerHTML = dds;
-    }
-};
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let dds = xhttp.responseText;
+            document.getElementById("resultado").innerHTML = dds;
+        }
+    };
 
-let dataAnoMesDia = document.getElementById("data").value;
-let data = formataData(dataAnoMesDia);
-xhttp.open("GET", PATH + data, true);
-xhttp.send();
+    let primeiraDataInput = document.getElementById("primeiraData").value;
+    let primeiraData = formataData(primeiraDataInput);
+    let segundaDataInput = document.getElementById("segundaData").value;
+    let segundaData = formataData(segundaDataInput);
+    xhttp.open("GET", obtemUrlRequest(primeiraData,segundaData), true);
+    xhttp.send();
 }
 
+/**
+ * Obter url que será usada para realizar a requisição
+ * @param {string} primeiraData primeira data no formato dd/MM/aaaa
+ *  que fará parte da url
+ * @param {string} segundaData segunda data no formato dd/MM/aaaa
+ *  que fará parte da url
+ * @return {string} url montada com as duas datas
+ */
+function obtemUrlRequest(primeiraData, segundaData){
+    const encodePrimeiraData = encodeURIComponent(primeiraData)
+    const encodeSegundaData = encodeURIComponent(segundaData)
+    return `${PATH}firstDate=${encodePrimeiraData}&secondDate=${encodeSegundaData}`;
+}
+
+/**
+ * Aplica o valor do dia de hoje aos campos de data
+ *
+ */
 function dataCorrente() {
-document.getElementById("data").valueAsDate = new Date();
+    document.getElementById("data").valueAsDate = new Date();
 }
 
-// Funções para integração (satisfazer contrato do servidor)
-
-function extraiDiaDaSemanaDaResposta(resposta) {
-return JSON.parse(resposta).diaDaSemana;
-}
-
-// Dia ou mês deve possuir dois dígitos
+/**
+ * Garante que o dia ou o Mes estarão no formato correto
+ * @param {number} n que é o numero que representa o dia ou mes
+ * @return {string} dia ou mes formatado com duas casas decimais
+ */
 function formataDiaOuMes(n) {
-return ("00" + n).substr(-2, 2);
+    return ("00" + n).substr(-2, 2);
 }
 
-// Ano deve possuir quatro dígitos
+/**
+ * Garante que ano estará no formato correto
+ * @param {number} n que é o numero que representa o ano
+ * @return {string} ano formatado com quatro casas decimais
+ */
 function formataAno(n) {
-return ("0000" + n).substr(-4,4);
+    return ("0000" + n).substr(-4, 4);
 }
 
-// ENTRADA: ano-mes-dia SAIDA: dd-mm-yyyy
+/**
+ * Formata a entrada de yyyy-MM-dd para dd/MM/yyyy
+ * 
+ * @param {string} data no formato yyyy-MM-dd que será formatada
+ * @return {string} data formata dd/MM/yyyy
+ */
 function formataData(data) {
-let [a, m, d] = data.split("-");
+    let [a, m, d] = data.split("-");
 
-let dia = formataDiaOuMes(d);
-let mes = formataDiaOuMes(m);
-let ano = formataAno(a);
+    let dia = formataDiaOuMes(d);
+    let mes = formataDiaOuMes(m);
+    let ano = formataAno(a);
 
-return `${d}-${m}-${a}`;
+    return `${d}/${m}/${a}`;
 }
+
+module.exports = {
+    formataData: formataData,
+    obtemUrlRequest: obtemUrlRequest
+};
